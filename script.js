54f0f8df9d2d07112f0a2612ebf003736f01f834
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-07-25 11:39:12 -0500

    add button to get user desired sqrs per grid
    - works when user uses the button to submit, doesn't work on first load without user input

--- script.js
+++ script.js
@@ -2,8 +2,27 @@
 
 const container = document.getElementById('container');
 const squares = document.getElementsByClassName('squares')
+const numOfSquaresBtn = document.getElementById('input')
+
 
 let grid = 32
+createGrid()
+draw()
+
+numOfSquaresBtn.addEventListener('click', getNumberofSquares)
+
+function getNumberofSquares (){
+    let grid = prompt("How many squares per side would you like? Max = 100");
+    if(grid <= 100){    
+    removeGrid();
+    createGrid(grid);
+    draw();
+    } else {
+        alert("Grid must be less than 100 per side");
+    }
+}
+
+
 
 function createGrid(grid){
     grid **=2
@@ -16,11 +35,20 @@ function createGrid(grid){
     }
 }
 
-createGrid(grid)
+// function remove the grid child elements
+function removeGrid(){
+    let gridContainer = document.getElementById('container');
+    last = gridContainer.lastElementChild;
+    while(last) {
+        last.remove();
+        last = gridContainer.lastElementChild
+    }
+}
 
 // * add “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would. */
-for(i = 0; i < squares.length; i++){
+function draw() {
+    for(i = 0; i < squares.length; i++){
     squares[i].addEventListener('mouseover', function (){
         this.style.backgroundColor = "red" // why did "this" work?
     } )
-}
+}}
